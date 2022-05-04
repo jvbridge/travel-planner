7b691c283995bc8a054ac31b5624538481af8e47
John Netzel <johnrnetzel@gmail.com>
2022-05-04 10:36:55 -0800

    Add routes to trip router

--- routes/api/tripRoutes.js
+++ routes/api/tripRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
+  
+router.post('/', async (req, res) => {
+    res.json(`${req.method} ${req.url}`);
+  });
 
-console.log("Trip router ready");
+router.delete('/:id', async (req, res) => {
+      res.json(`${req.method} ${req.url}`);
+  });
 
 module.exports = router;
